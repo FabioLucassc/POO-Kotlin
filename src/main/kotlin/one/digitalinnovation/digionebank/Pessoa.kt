c0006f814e218107a24c1934dd76e61b445f5c04
fabiolucassc <fabio-lucas1.000@hotmail.com>
2021-06-29 11:24:38 -0300

    Pessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,12 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Pessoa 1"
+    var cpf: String = "123.132.123-12"
+}
+
+fun main() {
+    val fabio = Pessoa()
+    println(fabio.nome)
+    println(fabio.cpf)
 }
