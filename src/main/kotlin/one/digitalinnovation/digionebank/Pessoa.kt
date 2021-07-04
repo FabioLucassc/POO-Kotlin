14cc7da26a0938f04b71db19e0291d4d31394b1f
fabiolucassc <fabio-lucas1.000@hotmail.com>
2021-07-03 22:16:05 -0300

    construtor de função

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,14 +3,15 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Pessoa 1"
     var cpf: String = "123.132.123-12"
-    private set
+        private set
+
+    constructor()
+
+    fun pesoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val fabio = Pessoa()
-    println(fabio.nome)
-    println(fabio.cpf)
-
-    // fabio.cpf = "54665"
+    println(fabio.pesoaInfo())
     println(fabio)
 }
