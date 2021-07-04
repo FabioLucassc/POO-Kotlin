ca38a31ec218cf3e01cea99f0bfabaee71fe8f3a
fabiolucassc <fabio-lucas1.000@hotmail.com>
2021-07-03 21:00:43 -0300

    Private Set

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,14 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Pessoa 1"
     var cpf: String = "123.132.123-12"
+    private set
 }
 
 fun main() {
     val fabio = Pessoa()
     println(fabio.nome)
     println(fabio.cpf)
+
+    // fabio.cpf = "54665"
+    println(fabio)
 }
